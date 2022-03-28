379aef3bb68c71a19b3dc36134db5884fee62597
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-03-29 00:34:12 +0600

    components make done

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>Do you want to know about us?</h2>
+            <p>First visit our page</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Friends/Friends.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            <h1>Hello friend how are You?</h1>
+            <p>Parle kico taka der dis..</p>
+        </div>
+    );
+};
+
+export default Friends;
