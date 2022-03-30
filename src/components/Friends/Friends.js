a738518b5d89b6d8088cdef50375a73056ae72e1
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-03-30 12:15:33 +0600

    navigate

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
+    const [friends, setFriends] = useState([])
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/users`)
+            .then(res => res.json())
+            .then(data => setFriends(data))
+    }, [])
     return (
         <div>
             <h1>Hello friend how are You?</h1>
-            <p>Parle kico taka der dis..</p>
+            <p>Parle kico taka der dis..{friends.length}</p>
+            {
+                friends.map(friend => <Friend
+                    key={friend.id}
+                    friend={friend}
+                ></Friend>)
+            }
         </div>
     );
 };
